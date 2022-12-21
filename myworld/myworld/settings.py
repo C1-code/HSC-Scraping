ce1e6ddc67793d2d062ae8d533f3ef4e4e0d4fc3
Eli <eliaschrisc1fp@gmail.com>
2022-12-21 17:31:16 +1100

    Fixed key leak

--- myworld/myworld/settings.py
+++ myworld/myworld/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(#z3gs9s$_84lyft9&&w687s12p9e^6rq$@vii#mrrjo0^v1zp'
+f = open("C:\\Users\\elias\\Desktop\\DjangoKey.txt", "r")
+content = f.readlines()
+SECRET_KEY = content[0]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
