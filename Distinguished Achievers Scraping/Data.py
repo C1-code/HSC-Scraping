bae2b9388b4d1822faac22b13691ad214be1644c
Eli <eliaschrisc1fp@gmail.com>
2022-12-28 12:40:55 -0500

    Took out unecessary functions for the distinguished achievers scraping

--- Distinguished Achievers Scraping/Data.py
+++ Distinguished Achievers Scraping/Data.py
@@ -6,11 +6,16 @@ import time
 import plotly.express as px
 import plotly
 import os
+import datetime as dt
 
 #All the lowercase letters as an array
 letters = string.ascii_lowercase
+
+#Ensuring the files are in a seperate folder to the code
 current_dir = os.path.dirname(__file__)
 pathToCSV = current_dir + "\\data\\"
+
+CURRENT_YEAR = dt.date.today().year
 
 def fileExists(path: str)->bool:
     '''Checks if the file given exists'''
@@ -89,9 +94,9 @@ def getDataFrame(year: int) -> pd.DataFrame:
         path = pathToCSV + path
         df = pd.read_csv(path, index_col=[0]).reset_index().drop(["index"], axis=1)
         return df
-    #The urls array will contain every link to go through and read the data from, the format of which changes from 2001-2015, 2016, 2017, 2018-2022
+    #The urls array will contain every link to go through and read the data from, the format of which changes from 2001-2015, 2016, 2017, 2018-CURRENT_YEAR
     urls = []
-    if 2018<=year and year<=2022:
+    if 2018<=year and year<=CURRENT_YEAR:
         for letter in string.ascii_lowercase:
             urls.append("https://educationstandards.nsw.edu.au/wps/portal/nesa/about/events/merit-lists/distinguished-achievers/" + str(year) +"/" + letter)
     elif year==2017:
@@ -168,146 +173,18 @@ def getDataFrame(year: int) -> pd.DataFrame:
     print("getDataFrame execution time: " + str(time.time()-t0))
     return maindf
 
-def generateCSVs()->None:
-    '''If the csv for a specific year doesn't exist, create it. Else retrieve it from local storage'''
-    for year in getYears():
-        getDataFrame(year)
-    
-def getList(year: int, item="School")->list:
-    '''Get either the school or subject list for a specified year'''
-    if item!="School" and item!="Subject":
-        raise ValueError("The item is not a suitable argument")
-    df = getDataFrame(year)
-    #main is the list of either the schools or subjects of a specific year
-    main = []
-    for focus in df[str(year) + " " + item]:
-        if focus not in main:
-            if year>=2015 and year<=2022:
-                main.append(focus.strip().strip("\n").strip("\r"))
-            if year>=2001 and year<=2014:
-                main.append(focus)
-    #Alphabetically sort it
-    main.sort()
-    return main
-
 def getYears():
-    '''Get all the years from 2001 to 2022 as a list for iteration'''
+    '''Get all the years from 2001 to CURRENT_YEAR as a list for iteration'''
     years = []
     year = 2001
-    while year<=2022:
+    while year<=CURRENT_YEAR:
         years.append(year)
         year+=1
     return years
 
-def successive(main: list):
-    '''Returns an array with the different components of the list concated together'''
-    #E.g. ["Math", "Ext", "1"] -> ["Math", "Math Ext", "Math Ext 1"]
-    successive = []
-    i = 0
-    while i<len(main):
-        j = 0
-        toAppend = ""
-        while j<=len(successive):
-            toAppend+=main[j] + " " 
-            j+=1
-        toAppend = toAppend.strip()
-        successive.append(toAppend)
-        i+=1
-    return successive
+def generateCSVs()->None:
+    '''If the csv for a specific year doesn't exist, create it. Else retrieve it from local storage'''
+    for year in getYears():
+        getDataFrame(year)
 
-def filter(year: int, rawInput: str, item="School")->str:
-    '''Given a user input and a year, find the subject the user was most likely trying to get'''
-    # We seperate the rawInput string into parts e.g.
-    # 'Math Ext 1' -> ['Math','Ext','1']
-    # Then check, for each subject, if 'Math', 'Ext', or '1' is in the subject
-    # Increase the "count" for that subject for each time any element in the rawInputs array appears
-    # The count is a measure of how likely it is that the subject is the subject the user is after 
-    if type(year)!=int or type(rawInput)!=str or type(item)!=str:
-        raise TypeError("One input into filter is not of the correct type")
-    
-    if item!="School" and item!="Subject":
-        raise ValueError("item parameter needs to be either 'School' or 'Subject'")
-
-    mainList = getList(year, item=item)
-    potentialMatches = []
-
-    rawInputs = rawInput.split(" ")
-    power = successive(rawInputs)
-    
-    #Assigning a 'count' score to each element in mainList
-    for element in mainList:
-        if rawInput.lower()==element.lower():
-            return element
-        count = 0 
-        for splited in rawInputs:
-            if splited.lower() in element.lower():
-                count+=element.lower().count(splited.lower())
-            
-            for success in power:
-                if element.lower().startswith(success.lower()):
-                    count+=1
-        if count>1:
-            potentialMatches.append([element, count])
-
-    if len(potentialMatches)==0:
-        print("No potential matches for the year")
-        return ""
-    
-    #Finding the element with the highest count. If there is no single element, return an empty string
-    maxCount = 0
-    finals = []
-    for element in potentialMatches:
-        if element[1]>maxCount:
-            finals.clear()
-            finals.append(element)
-            maxCount = element[1]
-        elif element[1]==maxCount:
-            finals.append(element)
-    
-    if len(finals)!=1:
-        print("School didn't exist in " + str(year) + ".")
-        return ""
-    
-    return finals[0][0]
-
-def getSchoolYearSubjectCount(year: int, school: str, subject: str):
-    '''Get the amount of band 6's for school in subject for the year'''
-    df = getDataFrame(year)
-
-    school = filter(year, school, item="School")
-    subject = filter(year, subject, item="Subject")
-
-    if school=="" or subject=="":
-        print("School or subject not identified")
-        return 0
-
-    # print("*****************")
-    # print(str(year))
-    # print(school)
-    # print(subject)
-    # print("*****************")
-    
-    count = 0
-    i = 0
-    while i<len(df[str(year) + " School"]):
-        if df[str(year) + " School"][i]==school:
-            if df[str(year) + " Subject"][i]==subject:
-                count+=1
-        i+=1
-    return count
-
-def getSchoolRecordSubject(school: str, subject: str):
-    years = getYears()
-    main = {"Years": [], "Band 6 Count": []}
-    for year in years:
-        main["Years"].append(year)
-        main["Band 6 Count"].append(getSchoolYearSubjectCount(year, school, subject))
-    scores = pd.DataFrame(main)
-    return scores, school, subject
-
-def graph(school: str, subject: str) -> str:
-    '''Returns the html code of the graph if it was a div object (used in index.html)'''
-    scores, schoolName, subjectName = getSchoolRecordSubject(school, subject)
-    fig = px.scatter(scores, x="Years", y="Band 6 Count", title=schoolName + " band 6 count for " + subjectName)
-    graphDiv = plotly.offline.plot(fig, auto_open=False, output_type="div")
-    return graphDiv
+generateCSVs()
